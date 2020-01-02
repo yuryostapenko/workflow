151206921c5655b548cb6f34f81adeb2d6e63d06
Yury Ostapenko <yuriy.ostapenko@gmail.com>
2020-01-02 14:11:24 +1000

    PROJECT-127 Look and say Test and Answer

--- look_and_say.js
+++ look_and_say.js
@@ -2,3 +2,13 @@
  * In mathematics, the look-and-say sequence is the sequence of integers beginning as follows:
  * 1, 11, 21, 1211, 111221, 312211, 13112221, 1113213211, ...
  */
+
+function a(n) {
+  return '';
+}
+
+// test
+console.log('test', a(7) === '1113213211');
+
+// result
+console.log('answer', a(8));
